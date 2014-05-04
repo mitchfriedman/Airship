bcb88b18e34c6b110204322f87e7c7280bef8b5c
KleptoKat <kleptokat@null.net>
2014-05-04 02:09:14 -0500

    Revert "Edited Level"
    This reverts commit 86c694b469c7457caaa2bf22e8ab225081ac6a68.

--- src/com/mitch/flyship/screens/Level.java
+++ src/com/mitch/flyship/screens/Level.java
@@ -1,70 +1,88 @@
 package com.mitch.flyship.screens;
 
+import android.util.Log;
+
 import com.mitch.flyship.Assets;
 import com.mitch.framework.Game;
 import com.mitch.framework.Graphics;
 import com.mitch.framework.Image;
 import com.mitch.framework.Screen;
 
-
 public class Level extends Screen {
-	static final int BACKGROUND_IMAGE_HEIGHT = 2336;
 	
 	Game game;
-	public int backgroundPos = 0;
+	public int background1Y, background2Y;
+	private int backgroundSpeed;
 	public Image backgroundImage;
-	private double speed;
+	
+	final int IMAGE_HEIGHT = 2336;
 	
 	public Level(Game game) {
 		super(game);
 		this.game = game;
-		backgroundPos = 0;
+		background1Y = 0;
+		background2Y = -IMAGE_HEIGHT;
 		
-		setBackgroundImage("background");
-		setSpeed(1.00);
+		setBackgroundImage();
 	}
 	
 	public void loadXML(String file) {
 		
 	}
-	
-	public void setBackgroundImage(String image) {
+	public void setBackgroundImage() {
 		/* parse XML and load background image */
 		Graphics g = game.getGraphics();
+		Assets.loadImage("background", "Backgrounds/sky.png", null, g);
 		backgroundImage = Assets.getImage("background");
+		setSpeed(1);
 	}
 	
-	public void setSpeed(double speed) {
-		this.speed = speed;
+	public void setSpeed(int speed) {
+		backgroundSpeed = speed;
 	}
-	public double getSpeed() {
-		return speed;
+	public int getSpeed() {
+		return backgroundSpeed;
 	}
 	
 	public void update(float deltaTime) {
+		background1Y += backgroundSpeed;
+		background2Y += backgroundSpeed;
 		
-		backgroundPos += getSpeed();
-		backgroundPos = backgroundPos > BACKGROUND_IMAGE_HEIGHT ? 0 : backgroundPos;
+		if(background1Y >= IMAGE_HEIGHT) {
+			background1Y -= 2 * IMAGE_HEIGHT;
+		}
+		if(background2Y >= IMAGE_HEIGHT) {
+			background2Y -= 2 * IMAGE_HEIGHT;
+		}
+		
+		
 	}
 	
 	public void paint(float deltaTime) {
 		Graphics g = game.getGraphics();
 		g.drawARGB( 255, 0, 0, 0);
-		g.drawImage(backgroundImage, 0, backgroundPos);
-		g.drawImage(backgroundImage, 0, backgroundPos-BACKGROUND_IMAGE_HEIGHT);
+		g.drawImage(backgroundImage, 0, background1Y);
+		g.drawImage(backgroundImage, 0, background2Y);
 	}
-	
-	
 	@Override
-	public void pause() {}
-	
+	public void pause() {
+		// TODO Auto-generated method stub
+		
+	}
 	@Override
-	public void resume() {}
-	
+	public void resume() {
+		// TODO Auto-generated method stub
+		
+	}
 	@Override
-	public void dispose() {}
-	
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
 	@Override
-	public void backButton() {}
+	public void backButton() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
